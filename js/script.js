5da88a725d0091699c1f829d69c2f86c6f19ddd3
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-09-14 19:21:34 +0200

    Change js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,39 @@
-$(function () {
-    const $listItems = $('.navbar-collapse li'),
-        $navbarHeader = $('.navbar-header');
-    $('.nav-tabs li:nth-child(2), .nav-tabs li:nth-child(4)').tooltip({
-        title: "We have new products!",
-        placement: "top",
-        trigger: "hover"
-    });
-    $('.contact button').tooltip({
-        title: "Send message to Us!",
-        placement: "top",
-        trigger: "hover"
-    });
-    $('.tooltip_').tooltip({
-        title: "Learn more about Our cookie policy!",
-        placement: "top",
-        trigger: "hover"
-    });
-    function removeActiveClass() {
-        $listItems.removeClass('active');
-        $navbarHeader.removeClass('active');
+const $listItems = $('.navbar-collapse li'),
+    $navbarHeader = $('.navbar-header');
+$('.nav-tabs li:nth-child(2), .nav-tabs li:nth-child(4)').tooltip({
+    title: "We have new products!",
+    placement: "top",
+    trigger: "hover"
+});
+$('.contact button').tooltip({
+    title: "Send message to Us!",
+    placement: "top",
+    trigger: "hover"
+});
+$('.tooltip_').tooltip({
+    title: "Learn more about Our cookie policy!",
+    placement: "top",
+    trigger: "hover"
+});
+function removeActiveClass() {
+    $listItems.removeClass('active');
+    $navbarHeader.removeClass('active');
+}
+$('.navbar-collapse a:not(.dropdown-toggle)').click(function () {
+    if ($('.navbar-collapse').hasClass('in')) {
+        $('.navbar-toggle').click();
     }
-    $('.navbar-collapse a:not(.dropdown-toggle)').click(function () {
-        if ($('.navbar-collapse').hasClass('in')) {
-            $('.navbar-toggle').click();
-        }
-        removeActiveClass();
-        $(this).parent().addClass('active');
-    });
-    $('.dropdown .dropdown-toggle').click(() => {
-        removeActiveClass();
-    });
-    $('.dropdown-menu a').click(function () {
-        $(`a[href="${$(this).attr('href')}"]`).tab('show');
-        $(this).parent().removeClass('active');
-    });
-    $('.navbar-header a').click(function () {
-        $(this).parent().addClass('active');
-        $listItems.removeClass('active');
-    });
+    removeActiveClass();
+    $(this).parent().addClass('active');
+});
+$('.dropdown .dropdown-toggle').click(() => {
+    removeActiveClass();
+});
+$('.dropdown-menu a').click(function () {
+    $(`a[href="${$(this).attr('href')}"]`).tab('show');
+    $(this).parent().removeClass('active');
+});
+$('.navbar-header a').click(function () {
+    $(this).parent().addClass('active');
+    $listItems.removeClass('active');
 });
